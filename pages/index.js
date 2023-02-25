26cf386a77700e4b205229469c9f0b7388c70a44
satishnavjivan <satish.dharavia@multicodes.in>
2023-02-25 12:20:12 +0530

    f project

--- pages/index.js
+++ pages/index.js
@@ -1,24 +1,10 @@
-/**
- * Internal Dependencies.
- */
-import Products from '../src/components/products';
-import { HEADER_FOOTER_ENDPOINT } from '../src/utils/constants/endpoints';
 
-/**
- * External Dependencies.
- */
-import axios from 'axios';
-import { getProductsData } from '../src/utils/products';
-import Layout from '../src/components/layout';
-
-export default function Home({ headerFooter, products }) {
+export default function Home() {
 	return (
-		<Layout headerFooter={headerFooter || {}}>
-			<Products products={products}/>
-		</Layout>
+		<><p>hi</p></>
 	)
 }
-
+/*
 export async function getStaticProps() {
 	
 	const { data: headerFooterData } = await axios.get( HEADER_FOOTER_ENDPOINT );
@@ -30,11 +16,7 @@ export async function getStaticProps() {
 			products: products ?? {}
 		},
 		
-		/**
-		 * Revalidate means that if a new request comes to server, then every 1 sec it will check
-		 * if the data is changed, if it is changed then it will update the
-		 * static file inside .next folder with the new data, so that any 'SUBSEQUENT' requests should have updated data.
-		 */
+		
 		revalidate: 1,
 	};
-}
+}*/
